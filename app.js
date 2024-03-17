aad1dd7481df4dd53751efdcbaa8dc8a02f59009
hanjinu <wlsdn1232580@gmail.com>
2024-03-17 14:58:26 +0900

    test

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+ 
+app.set("views", "./views");
+ // views(html)을 지정한 경로에서 불러온다.
+app.set("view engine", "ejs");
+
+const home = require("./routes/home");
+//경로에 있는 파일을 home으로 저장함
+app.use("/", home);
+//use -> 미들웨어를 등록하는 메소드
+
+module.exports = app;
