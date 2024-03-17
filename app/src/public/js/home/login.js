fa65d1e879766b87c35df6dd549ab48fc28cd846
hanjinu <wlsdn1232580@gmail.com>
2024-03-17 15:43:35 +0900

    id,pw connect

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use state"
 
-console.log("hello");
+// DOM -> Document Object Model
+const Id = document.querySelector("#Id"),//선택자
+    Pw = document.querySelector("#Pw"),
+    loginBtn = document.querySelector("button");
+
+// 위에서 dom이 id를 가져오기 전에 console.log가 먼져 출력됨
+
+loginBtn.addEventListener("click",login);
+
+function login() {
+    const req = {
+        id: Id.value,
+        psword : Pw.value,
+    }
+    console.log(req)
+}
