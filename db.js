089cef08d9707dc94b8401dd3cd1dd416e48f7ca
hanjinu <wlsdn1232580@gmail.com>
2024-03-30 11:23:46 +0900

    comment

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 // sequelize DB 연결 테스트
 (async () => {
     try {
-        await sequelize.authenticate();
+        await sequelize.authenticate(); //sequelize에 인증될때 까지 대기
         console.log('DB Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
