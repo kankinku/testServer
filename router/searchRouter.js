f020a5bfa21b49b30209fe5410bcb94057210ece
hanjinu <wlsdn1232580@gmail.com>
2024-05-12 17:55:42 +0900

    user_ID update

--- router/searchRouter.js
+++ router/searchRouter.js
@@ -13,13 +13,14 @@ router.get('/', (req, res) => {
 
         // 클라이언트로부터 받은 갤러리 아이디
         const gallery_Id = req.query.gallery_Id;
+        const user_Id = req.query.user_Id;
 
-        if (!gallery_Id) {
+        if (!gallery_Id && !user_Id) {
             // 클라이언트로 갤러리 아이디 목록 반환
             res.json(galleryIds);
         } else {
             // 클라이언트로부터 받은 갤러리 아이디를 이용하여 이미지 제목들을 가져오는 함수 호출
-            fetchImageId(gallery_Id, (error, results) => {
+            fetchImageId(gallery_Id, user_Id,(error, results) => {
                 if (error) {
                     res.status(500).json({ error: 'Internal Server Error' });
                     return;
@@ -51,17 +52,18 @@ function fetchGalleryIds(callback) {
 }
 
 // 이미지 제목들을 가져오는 함수
-function fetchImageId(galleryId, callback) {
+
+function fetchImageId(galleryId , userId , callback) {
     // gallery 테이블에서 선택한 gallery_Id 값을 갖는 목록의 Id 값을 가져오는 쿼리
     const query = `
     SELECT images.image_Id
     FROM gallery
     INNER JOIN images ON gallery.gallery_Id = images.gallery_Id
-    WHERE gallery.gallery_Id = ?;
+    WHERE gallery.gallery_Id = ? AND gallery.user_Id = ?;
   `;
 
     // 쿼리 실행
-    connection.query(query, [galleryId], (err, results) => {
+    connection.query(query, [galleryId, userId], (err, results) => {
         if (err) {
             console.error('쿼리 실행 오류:', err);
             callback(err, null);
